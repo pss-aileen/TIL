6a80115b1ac48e4b21ba31a8b260b73713834113
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-05-27 00:43:58 +0900

    0:43

--- test.js
+++ test.js
@@ -1,16 +1,5 @@
-<div align="center">
+"use strict";
 
-<img width="160" src="assets/octcat.png" alt="TIL" />
-
-# TIL 
-Today I Learn and Study Log.
-
-This image was made with [My Octocat](https://myoctocat.com/)
-</div>
-
----
-
-```javascript
 const ProgrammingStudyTime = [
   {
     period: "2023/11/18~2023/12/31",
@@ -43,31 +32,5 @@ const ProgrammingStudyTime = [
     // hoursTotal: "??",
   },
 ];
-```
 
-
-## Study Time
-### Programming
-- 2023/11/18〜12/31 => 116.75h / 116.75h
-- January => 93.75h / 210.50h
-- February => 95.25h / 306.25h
-- March => 90.50h / 396.75h
-- April => 21.25h / 418.0h
-- May => 
-- June => 
-- July => 
-- August => 
-- September => 
-- October => 
-- November => 
-- December => 
-
-### English
-- May => 
-- June => 
-- July => 
-- August => 
-- September => 
-- October => 
-- November => 
-- December => 
+console.log(ProgrammingStudyTime[0]["hoursPerMonth"] + ProgrammingStudyTime[1]["hoursPerMonth"]);
