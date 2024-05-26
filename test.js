81c2cebc4af1acfc2b6153597dea38b7eb2a28f3
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-05-27 01:04:50 +0900

    🛠️ fix: write codes at README (#28)
    * 0:24
    
    * 0:27
    
    * 0:28
    
    * 0:29
    
    * 0:33
    
    * 0:43
    
    * 0:46
    
    * 0:54
    
    * 1:03

---
+++ test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const ProgrammingStudyTime = [
+  {
+    period: "2023/11/18~2023/12/31",
+    hoursPerMonth: 116.75,
+    // hoursTotal: 116.75,
+  },
+  {
+    period: "2024/01/01~2024/01/31",
+    hoursPerMonth: 93.75,
+    // hoursTotal: 210.50,
+  },
+  {
+    period: "2024/02/01~2024/02/29",
+    hoursPerMonth: 95.25,
+    // hoursTotal: 306.25,
+  },
+  {
+    period: "2024/03/01~2024/03/31",
+    hoursPerMonth: 90.50,
+    // hoursTotal: 396.75,
+  },
+  {
+    period: "2024/04/01~2024/04/31",
+    hoursPerMonth: 21.25,
+    // hoursTotal: 418.00,
+  },
+  {
+    period: "2024/05/01~2024/05/31",
+    hoursPerMonth: "??",
+    // hoursTotal: "??",
+  },
+];
+
+console.log(ProgrammingStudyTime[0]["hoursPerMonth"] + ProgrammingStudyTime[1]["hoursPerMonth"]);
