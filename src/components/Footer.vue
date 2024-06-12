87cfc34ab13106419d1e5fa212ab8bcf9ebf0d9f
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-06-13 00:13:51 +0900

    📚 update: daily report 240612
    * 14:23
    
    Bootstrap 0.5h
    
    * 勉強方法は変わる本 心理学からのアドバイス
    
    わからない、苦手な点を集中的にやる。勉強量より、何を学んだのか、昨日と比べて何ができるようになったかを意識する。「時間」を気にしない！！
    
    * delete vue project
    
    時間と決別するために削除！
    
    * Bootstrap 5 ざっと勉強
    
    モバイルファースト、グリッドあたりの理解、col、rowとレスポンシブができる自信がないのでそのあたりを頑張る必要がある。

--- src/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <footer class="footer footer-center p-4 bg-base-200 text-base-content mt-12">
-    <aside>
-      <p>Copyright © 2024 - All right reserved by Aileen TIL</p>
-    </aside>
-  </footer>
-</template>
--- src/components/Navigation.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="navbar bg-base-100">
-    <a class="btn btn-ghost text-xl" href="#">TIL</a>
-  </div>
-</template>
